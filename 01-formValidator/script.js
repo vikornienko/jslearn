773fd4e88ae8db915dbb2ccea805272352c418e0
vikornienko <vikornienko76@gmail.com>
2025-09-17 20:57:31 +0500

    Доработка файла script.js в проекте по валидации формы.

--- 01-formValidator/script.js
+++ 01-formValidator/script.js
@@ -4,6 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Функция для вывода сообщения об ошибке
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
@@ -11,17 +12,23 @@ function showError(input, message) {
     small.innerText = message;
 }
 
+// Функция для вывода сообщения об успешном заполнении поля
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
 
+// Функция для проверки корректности электронной почты
 function emailIsValid(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    return emailRegex.test(String(email).toLowerCase());
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRegex.test(email.value.trim())) {
+        showSuccess(email);
+    } else {
+        showError(email, 'Электронная почта указана не корректно.');
+    }
 }
 
+// Функция для проверки обязательного заполнения полей
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
         if(input.value.trim() === '') {
@@ -32,13 +39,36 @@ function checkRequired(inputArr) {
     });
 }
 
+// Функция для проверки длинны введенного значения
+function checkLength(input, min, max) {
+    if(input.value.lenghth < min) {
+        showError(input, `Значение ${getFieldName(input)} должно быть больше ${min} символов.`);
+    } else if(input.value.lenghth > max) {
+        showError(input, `Значение ${getFieldName(input)} должно быть меньше ${max} символов.`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Функция для проверки совпадения пароля.
+function checkPasswords(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Пароль не совпадает.')
+    }
+}
+
+// Функция для получения имени поля
 function getFieldName(input) {
-    return input.id.toUpperCase();
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Слушатель событий
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    checkRequired([username, email, password, password2]);    
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 8, 16);
+    emailIsValid(email);
+    checkPasswords(password, password2);    
 })
